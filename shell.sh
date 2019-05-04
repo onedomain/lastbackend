eb793a1eabb0e3f1e2318b55d411a90b348a475d
OneDomain <admin@onedoma.in>
2019-05-04 05:45:32 -0400

    updated image urls

---
+++ shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker rmi 5b1816ee4017
+docker rmi 7e9ae381ca60
+docker rmi f06d7c8b6d42
+docker rmi e76c7a479bc2
+docker rmi 9a4c5c657731
+docker rmi c9835a5eb3dc
+docker rmi 815a718b1252
+docker rmi 598dcc75d0bf
+docker rmi a1305971570f
+docker rmi b4f58de16586
+docker rmi 68ac0400451e
+docker rmi d917bf49b9a7
+docker rmi babdad83c0fb
